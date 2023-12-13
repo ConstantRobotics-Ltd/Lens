22434c863be34a6e7c1022791f9b94f28627dec4
ayikahmet <a.ayik@constantroboticscom>
2023-12-13 15:12:02 +0100

    Update version

--- src/LensVersion.h
+++ src/LensVersion.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define LENS_MAJOR_VERSION 4
-#define LENS_MINOR_VERSION 3
+#define LENS_MINOR_VERSION 4
 #define LENS_PATCH_VERSION 1
 
-#define LENS_VERSION "4.3.1"
+#define LENS_VERSION "4.4.1"
